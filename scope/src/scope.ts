1199924d009998b073a30fdf8f2d1f264d88dbc3
Autumn Russell <autumn@adrusi.com>
2025-07-24 23:10:48 -0700

    [scope] remaining hooks

--- scope/src/scope.ts
+++ scope/src/scope.ts
@@ -1,19 +1,26 @@
 import { ErrorGroup } from "@cantrip/error"
 
-class InterruptError extends Error {}
+export class InterruptError extends Error {}
 
+const SCOPE_GUARD = Symbol("SCOPE_GUARD")
 const SCOPE_NOTIFY = Symbol("SCOPE_NOTIFY")
 const SCOPE_ON_HALT = Symbol("SCOPE_ON_HALT")
 const SCOPE_OFF_HALT = Symbol("SCOPE_OFF_HALT")
 const SCOPE_ON_COMPLETE = Symbol("SCOPE_ON_COMPLETE")
 const SCOPE_ERRORS = Symbol("SCOPE_ERRORS")
 
-class Scope {
+export class Scope {
   #halted: boolean = false
   #completeListeners: Set<() => void> = new Set()
   #procs: Set<Promise<void>> = new Set()
   #haltListeners: Set<(error: InterruptError) => void> = new Set();
   [SCOPE_ERRORS]: Set<unknown> = new Set()
+
+  constructor(guard: typeof SCOPE_GUARD) {
+    if (guard !== SCOPE_GUARD) {
+      throw new Error("Illegal invocation of Scope constructor")
+    }
+  }
 
   spawn(callback: () => Promise<void>) {
     const callingScope = currentScope
@@ -81,7 +88,7 @@ export function withScope(
   let callingScope = currentScope
 
   return new Promise((resolve, reject) => {
-    let scope = new Scope()
+    let scope = new Scope(SCOPE_GUARD)
     scope[SCOPE_ON_COMPLETE](() => {
       if (scope[SCOPE_ERRORS].size !== 0) {
         reject(new ErrorGroup(Array.from(scope[SCOPE_ERRORS])))
@@ -127,6 +134,70 @@ export async function awaitHook<A>(p: PromiseLike<A>): Promise<A> {
   })
 }
 
-// export function forAwaitHook<A>(iter: AsyncIterable<A>): AsyncIterable<A> {
+export function forAwaitHook<A, Return = unknown, Next = unknown>(
+  iter: AsyncIterable<A> | AsyncIterator<A, Return, Next>,
+): AsyncIterator<A, Return, Next> {
+  if (Symbol.asyncIterator in iter) {
+    iter = iter[Symbol.asyncIterator]()
+  }
 
-// }
+  if (currentScope === null) return iter
+
+  return new HookedAsyncIterator(iter)
+}
+
+class HookedAsyncIterator<A, Return, Next>
+  implements AsyncIterator<A, Return, Next>
+{
+  #iter: AsyncIterator<A, Return, Next>
+
+  return?: (
+    value?: Return | PromiseLike<Return> | undefined,
+  ) => Promise<IteratorResult<A, Return>>
+
+  throw?: (e?: any) => Promise<IteratorResult<A, Return>>
+
+  constructor(iter: AsyncIterator<A, Return, Next>) {
+    this.#iter = iter
+
+    if (typeof iter.return === "function") {
+      this.return = async (
+        value?: Return | PromiseLike<Return> | undefined,
+      ) => {
+        return await awaitHook(iter.return!(value))
+      }
+    }
+
+    if (typeof iter.throw === "function") {
+      this.throw = async (e?: any) => {
+        return await awaitHook(iter.throw!(e))
+      }
+    }
+  }
+
+  async next(...args: [] | [Next]): Promise<IteratorResult<A, Return>> {
+    return await awaitHook(this.#iter.next(...args))
+  }
+}
+
+export function awaitUsingHook<A extends AsyncDisposable>(res: A): A {
+  // we either need to proxy this object or mutate it
+  // neither is great, but i think mutating is less likely to break consuming code?
+
+  // return new Proxy(res, {
+  //   get(_target, prop, _receiver) {
+  //     if (prop === Symbol.asyncDispose) {
+  //       return async () => {
+  //         await awaitHook(res[Symbol.asyncDispose]())
+  //       }
+  //     }
+  //     return Reflect.get(_target, prop, _receiver)
+  //   }
+  // })
+
+  const origDispose = res[Symbol.asyncDispose]
+  res[Symbol.asyncDispose] = async function () {
+    await awaitHook(origDispose.call(this))
+  }
+  return res
+}
