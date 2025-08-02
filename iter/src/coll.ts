b4d045d48298dfb6548ee5e6b77d8c493ba8d408
Autumn Russell <autumn@adrusi.com>
2025-08-02 15:07:33 -0700

    [iter] fix circular dependency

--- iter/src/coll.ts
+++ iter/src/coll.ts
@@ -1,13 +1,13 @@
-import type * as compat from "@cantrip/compat/iter"
-import * as iter from "./iter"
+import * as compat from "@cantrip/compat/iter"
 
-export class ArrayIter<A> extends iter.BackSizeIter<A> {
+export class ArrayIter<A> implements compat.BackSizeIterator<A> {
+  public readonly [compat.IS_ITERATOR] = true
+
   private readonly src: readonly A[]
   private cursor: number
   private cursorBack: number
 
   public constructor(src: readonly A[]) {
-    super()
     this.src = src
     this.cursor = 0
     this.cursorBack = src.length - 1
@@ -18,12 +18,12 @@ export class ArrayIter<A> extends iter.BackSizeIter<A> {
     return { done: false, value: this.src[this.cursor++] }
   }
 
-  public nextBack(): compat.IteratorResult<A> {
+  public [compat.NEXT_BACK](): compat.IteratorResult<A> {
     if (this.cursorBack < this.cursor) return { done: true, value: undefined }
     return { done: false, value: this.src[this.cursorBack--] }
   }
 
-  public size(): number {
+  public [compat.SIZE](): number {
     return this.cursorBack - this.cursor + 1
   }
 }
